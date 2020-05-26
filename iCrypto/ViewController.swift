094811346643a9b2c8cfc8c06a4a30cd2b76431a
Nghia Nguyen <nghianguyen@teqnological.asia>
2020-05-26 23:54:45 +0700

    Init Project

---
+++ iCrypto/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iCrypto
+//
+//  Created by Nghia Nguyen on 5/26/20.
+//  Copyright © 2020 Nghia Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
